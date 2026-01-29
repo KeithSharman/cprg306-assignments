50bf7d76c5424737378f325d6abf051eba4be4b2
KeithSharman <Keithsharman3@gmail.com>
2026-01-28 18:17:21 -0700

    Update studentInfo.tsx

--- app/week-2/studentInfo.tsx
+++ app/week-2/studentInfo.tsx
@@ -4,7 +4,7 @@ export default function StudentInfo () {
     return (
         <>
             <h1>Keith Sharman</h1>
-            <Link href="https://github.com/KeithSharman">Github Link "https://github.com/KeithSharman" </Link>
+            <Link href="https://github.com/KeithSharman">Github Link https://github.com/KeithSharman </Link>
         </>
     )
 }
